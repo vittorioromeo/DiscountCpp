990ba6ef30a6951c508bdcddc841a29f462c6e15
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-10-02 14:33:44 +0200

    automatic clang-format

--- include/DiscountCpp/DiscountCpp.hpp
+++ include/DiscountCpp/DiscountCpp.hpp
@@ -7,119 +7,183 @@
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
-extern "C"
-{
-	#include <mkdio.h>
+extern "C" {
+#include <mkdio.h>
 }
 
 namespace discountcpp
 {
-	namespace Impl
-	{
-		long getFilePtrSize(FILE* mFilePtr)
-		{
-			if(fseek(mFilePtr, 0, SEEK_END)) throw std::runtime_error("[DiscountCpp internal error] Error reading file ptr size");
-			auto size(ftell(mFilePtr));
-			if(size < 0 || fseek(mFilePtr, 0, SEEK_SET)) throw std::runtime_error("[DiscountCpp internal error] Error reading file ptr size");
-			return size;
-		}
+namespace Impl
+{
+    long getFilePtrSize(FILE* mFilePtr)
+    {
+        if(fseek(mFilePtr, 0, SEEK_END))
+            throw std::runtime_error(
+            "[DiscountCpp internal error] Error reading file ptr size");
+        auto size(ftell(mFilePtr));
+        if(size < 0 || fseek(mFilePtr, 0, SEEK_SET))
+            throw std::runtime_error(
+            "[DiscountCpp internal error] Error reading file ptr size");
+        return size;
+    }
 
-		std::size_t filePtrContentsToCString(char** mCString, FILE* mFilePtr)
-		{
-			std::size_t read{0};
-			*mCString = nullptr;
+    std::size_t filePtrContentsToCString(char** mCString, FILE* mFilePtr)
+    {
+        std::size_t read{0};
+        *mCString = nullptr;
 
-			if(!mFilePtr) return read;
+        if(!mFilePtr) return read;
 
-			long size{getFilePtrSize(mFilePtr)};
-			if(size < 0) return read;
+        long size{getFilePtrSize(mFilePtr)};
+        if(size < 0) return read;
 
-			*mCString = static_cast<char*>(malloc((std::size_t)size + 1));
-			if(!*mCString) return read;
+        *mCString = static_cast<char*>(malloc((std::size_t)size + 1));
+        if(!*mCString) return read;
 
-			read = fread(*mCString, 1, (std::size_t)size, mFilePtr);
-			(*mCString)[read] = 0;
-			*mCString = static_cast<char*>(realloc(*mCString, read + 1));
+        read = fread(*mCString, 1, (std::size_t)size, mFilePtr);
+        (*mCString)[read] = 0;
+        *mCString = static_cast<char*>(realloc(*mCString, read + 1));
 
-			if(size != static_cast<long>(read)) throw std::runtime_error("[DiscountCpp internal error] Error reading file ptr contents");
+        if(size != static_cast<long>(read))
+            throw std::runtime_error(
+            "[DiscountCpp internal error] Error reading file ptr contents");
 
-			return read;
-		}
+        return read;
+    }
 
-		inline std::string getFilePtrContents(FILE* mFilePtr) { char* buf; filePtrContentsToCString(&buf, mFilePtr); std::string result{buf}; free(buf); return result; }
-		inline std::string readFile(const std::string& mPath)
-		{
-			std::ifstream ifs{mPath};
-			return {std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>()};
-		}
-	}
+    inline std::string getFilePtrContents(FILE* mFilePtr)
+    {
+        char* buf;
+        filePtrContentsToCString(&buf, mFilePtr);
+        std::string result{buf};
+        free(buf);
+        return result;
+    }
+    inline std::string readFile(const std::string& mPath)
+    {
+        std::ifstream ifs{mPath};
+        return {
+        std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>()};
+    }
+}
 
-	class Document;
+class Document;
 
-	class Source
-	{
-		friend Document;
+class Source
+{
+    friend Document;
 
-		private:
-			std::string source;
-			MMIOT* mmiot{nullptr};
+private:
+    std::string source;
+    MMIOT* mmiot{nullptr};
 
-			inline void refreshMMIOT()
-			{
-				if(mmiot != nullptr) mkd_cleanup(mmiot);
-				const auto& cstr(source.c_str());
-				mmiot = mkd_string(cstr, strlen(cstr), 0);
-			}
-			inline MMIOT* getMMIOT() const
-			{
-				if(mmiot == nullptr) throw std::runtime_error("[DiscountCpp internal error] Source mmiot is null");
-				return mmiot;
-			}
+    inline void refreshMMIOT()
+    {
+        if(mmiot != nullptr) mkd_cleanup(mmiot);
+        const auto& cstr(source.c_str());
+        mmiot = mkd_string(cstr, strlen(cstr), 0);
+    }
+    inline MMIOT* getMMIOT() const
+    {
+        if(mmiot == nullptr)
+            throw std::runtime_error(
+            "[DiscountCpp internal error] Source mmiot is null");
+        return mmiot;
+    }
 
-		public:
-			~Source() { if(mmiot != nullptr) mkd_cleanup(mmiot); }
+public:
+    ~Source()
+    {
+        if(mmiot != nullptr) mkd_cleanup(mmiot);
+    }
 
-			inline static Source fromFile(const std::string& mPath)		{ Source result; result.readFromFile(mPath); return result; }
-			inline static Source fromStr(const std::string& mStr)	{ Source result; result.readFromString(mStr); return result; }
+    inline static Source fromFile(const std::string& mPath)
+    {
+        Source result;
+        result.readFromFile(mPath);
+        return result;
+    }
+    inline static Source fromStr(const std::string& mStr)
+    {
+        Source result;
+        result.readFromString(mStr);
+        return result;
+    }
 
-			inline void readFromFile(const std::string& mPath)		{ source = Impl::readFile(mPath); refreshMMIOT(); }
-			inline void readFromString(const std::string& mSource)	{ source = mSource; refreshMMIOT(); }
-			inline const std::string& getSource() const				{ return source; }
-	};
+    inline void readFromFile(const std::string& mPath)
+    {
+        source = Impl::readFile(mPath);
+        refreshMMIOT();
+    }
+    inline void readFromString(const std::string& mSource)
+    {
+        source = mSource;
+        refreshMMIOT();
+    }
+    inline const std::string& getSource() const { return source; }
+};
 
-	class Document
-	{
-		private:
-			const Source& source;
+class Document
+{
+private:
+    const Source& source;
 
-		public:
-			Document(const Source& mSource) : source(mSource) { }
+public:
+    Document(const Source& mSource) : source(mSource) {}
 
-			void writeHTMLToFile(const std::string& mPath)
-			{
-				FILE* file{fopen(mPath.c_str(), "w")};
-				markdown(source.getMMIOT(), file, 0);
-				fclose(file);
-			}
-			void writeHTMLToString(std::string& mStr)
-			{
-				FILE* file{tmpfile()};
-				markdown(source.getMMIOT(), file, 0);
-				mStr = Impl::getFilePtrContents(file);
-				fclose(file);
-			}
-	};
+    void writeHTMLToFile(const std::string& mPath)
+    {
+        FILE* file{fopen(mPath.c_str(), "w")};
+        markdown(source.getMMIOT(), file, 0);
+        fclose(file);
+    }
+    void writeHTMLToString(std::string& mStr)
+    {
+        FILE* file{tmpfile()};
+        markdown(source.getMMIOT(), file, 0);
+        mStr = Impl::getFilePtrContents(file);
+        fclose(file);
+    }
+};
 
-	namespace Impl
-	{
-		inline std::string getHTMLFromMarkdownSource(const Source& mSource)				{ std::string result; Document{mSource}.writeHTMLToString(result); return result; }
-		inline void writeToFile(const std::string& mPath, const std::string& mContents)	{ std::ofstream ofs{mPath}; ofs << mContents; ofs.flush(); ofs.close(); }
-	}
+namespace Impl
+{
+    inline std::string getHTMLFromMarkdownSource(const Source& mSource)
+    {
+        std::string result;
+        Document{mSource}.writeHTMLToString(result);
+        return result;
+    }
+    inline void writeToFile(
+    const std::string& mPath, const std::string& mContents)
+    {
+        std::ofstream ofs{mPath};
+        ofs << mContents;
+        ofs.flush();
+        ofs.close();
+    }
+}
 
-	inline std::string getHTMLFromMarkdownString(const std::string& mMarkdownString)	{ return Impl::getHTMLFromMarkdownSource(Source::fromStr(mMarkdownString)); }
-	inline std::string getHTMLFromMarkdownFile(const std::string& mMarkdownFilePath)	{ return Impl::getHTMLFromMarkdownSource(Source::fromFile(mMarkdownFilePath)); }
-	inline void writeHTMLFileFromMarkdownString(const std::string& mHTMLFilePath, const std::string& mMarkdownString)	{ Impl::writeToFile(mHTMLFilePath, getHTMLFromMarkdownString(mMarkdownString)); }
-	inline void writeHTMLFileFromMarkdownFile(const std::string& mHTMLFilePath, const std::string& mMarkdownFilePath)	{ Impl::writeToFile(mHTMLFilePath, getHTMLFromMarkdownFile(mMarkdownFilePath)); }
+inline std::string getHTMLFromMarkdownString(const std::string& mMarkdownString)
+{
+    return Impl::getHTMLFromMarkdownSource(Source::fromStr(mMarkdownString));
+}
+inline std::string getHTMLFromMarkdownFile(const std::string& mMarkdownFilePath)
+{
+    return Impl::getHTMLFromMarkdownSource(Source::fromFile(mMarkdownFilePath));
+}
+inline void writeHTMLFileFromMarkdownString(
+const std::string& mHTMLFilePath, const std::string& mMarkdownString)
+{
+    Impl::writeToFile(
+    mHTMLFilePath, getHTMLFromMarkdownString(mMarkdownString));
+}
+inline void writeHTMLFileFromMarkdownFile(
+const std::string& mHTMLFilePath, const std::string& mMarkdownFilePath)
+{
+    Impl::writeToFile(
+    mHTMLFilePath, getHTMLFromMarkdownFile(mMarkdownFilePath));
+}
 }
 
 #endif
